30efc0d342df0d1b7aafeefa8a01df3f28e7fc93
basispice <basil.breton@gmail.com>
2022-04-01 15:12:21 -0700

    removed extra paths

--- the_spot/wsgi.py
+++ the_spot/wsgi.py
@@ -15,8 +15,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_spot.settings')
 
 application = get_wsgi_application()
 
-application = WhiteNoise(application, root="/path/to/static/files")
-application.add_files("/path/to/more/static/files", prefix="more-files/")
+
 # import os
 
 # from django.core.wsgi import get_wsgi_application
